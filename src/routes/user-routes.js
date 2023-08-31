e8a8eb785870d50f3eb976a48fa70459f4327fab
myeonghwan <1jmhcho@gmail.com>
2023-08-31 17:36:48 +0900

    라우터 수정

--- src/routes/user-routes.js
+++ src/routes/user-routes.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const HttpStatus= require('http-status');
 const router = express.Router();
 const userController = require('../controllers/user-controller');
 
 router.post('/login', userController.login);
 router.post('/register', userController.register);
-
+router.get('/',function(req, res){
+    res.status(HttpStatus.OK).send({
+        status: HttpStatus.OK,
+        message: '서버 접속에 성공하였습니다.'
+})});
 module.exports = router;
